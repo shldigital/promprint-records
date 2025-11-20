d9d0b7afa0fb092298e6d7b05da7c462cb0152b5
N. Seymour-Smith <nrs23@sussex.ac.uk>
2025-11-20 14:18:55 +0000

    [fix] Add host ip to allowed hosts

--- promprint/settings.py
+++ promprint/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = [os.environ.get('HOSTNAME', ''), '127.0.0.1']
+ALLOWED_HOSTS = [
+    os.environ.get('HOSTNAME', ''),
+    os.environ.get('HOSTIP', ''),
+    '127.0.0.1',
+]
 CSRF_TRUSTED_ORIGINS = [f'https://{os.environ.get('HOSTNAME', 'localhost')}']
 
 # Application definition
